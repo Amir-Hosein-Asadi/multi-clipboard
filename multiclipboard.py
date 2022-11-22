e389e4acab0737fcabdc59f8c86d423a2254fa17
Amirhosein Asadi <amiroseinmis@gmail.com>
2022-11-22 15:24:16 +0330

    changed

--- multiclipboard.py
+++ multiclipboard.py
@@ -2,7 +2,9 @@ import sys
 import clipboard
 import json
 
-data = clipboard.paste()
-print(data)
+#data = clipboard.paste()
+#print(data)
 
 #clipboard.copy("abs")
+
+#run this python script directly  
