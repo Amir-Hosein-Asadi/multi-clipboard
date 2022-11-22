7c9de5afe58c3265d109bd1853c6b2b1516f4e11
Amirhosein Asadi <amiroseinmis@gmail.com>
2022-11-22 18:01:43 +0330

    change

--- multiclipboard.py
+++ multiclipboard.py
@@ -14,6 +14,32 @@ import json
 #-----------------------------------------------------------------------------
 #if statement that checks exactly one command
 
+#if len(sys.argv) == 2:
+#    command = sys.argv[1]
+#    print(command)
+
+def save_items(filepath, data):
+    with open(filepath , 'w') as f:
+        json.dump(data, f)
+        
+save_items("test.json", {"key": "value"})
+
+def load_items(filepath):
+    with open(filepath , 'r') as f:
+        data = json.load(f)
+        return data
+
 if len(sys.argv) == 2:
     command = sys.argv[1]
-    print(command)
+    
+    if command == "save":
+        print(command)
+    elif command == "load":
+        print(command)
+    elif command == "list":
+        print(command)
+    else:
+        print("Unknown command")
+
+else:
+    print("Please write exactly one command.")
